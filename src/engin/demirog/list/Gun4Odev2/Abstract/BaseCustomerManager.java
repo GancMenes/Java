e33dfe8e62413f1424eba348cb62f50c81c2f6b2
Ganc Serdar Menesyan <ganc_menesyan@hotmail.com>
2022-03-28 16:06:42 +0300

    Add files via upload

---
+++ src/engin/demirog/list/Gun4Odev2/Abstract/BaseCustomerManager.java
@@ -0,0 +1,8 @@
+package engin.demirog.list.Gun4Odev2.Abstract;
+
+import org.w3c.dom.ls.LSOutput;
+
+public abstract class BaseCustomerManager implements CustomerService{
+
+
+}
---
+++ src/engin/demirog/list/Gun4Odev2/Abstract/CustomerCheckService.java
@@ -0,0 +1,8 @@
+package engin.demirog.list.Gun4Odev2.Abstract;
+
+import engin.demirog.list.Gun4Odev2.Entities.Customer;
+
+public interface CustomerCheckService {
+
+    boolean checkIfReal(Customer customer);
+}
